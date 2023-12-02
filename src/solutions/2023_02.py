1461ad181e43f2487a734caccc03b34ad1311ec6
Aveenash Manoharan <46872605+Avee217@users.noreply.github.com>
2023-12-02 15:19:17 +0530

    Day 2- part a and and part b solutions

---
+++ src/solutions/2023_02.py
@@ -0,0 +1,26 @@
+from utils import *
+from collections import defaultdict 
+@answer_part_a(year=2023, day=2)
+def part_1(lines):
+    part_a=0
+    part_b=0
+    for line in lines:
+        flag = True
+        id , game = line.split(':')
+        Val=defaultdict(int)
+        for turn in game.split(';'):
+            for balls in turn.split(','):
+                n, color = balls.split()
+                if int(n)> {'red':12,'green':13, 'blue':14}.get(color,0):
+                    flag = False
+                n = int(n)
+                Val[color] = max(Val[color],n)
+        score = 1
+        for v in Val.values():
+            score*=v
+        part_b +=score
+        if flag==True:
+            part_a += int(id.split()[-1])
+            #print(id)
+    #return(part_a)
+    return(part_b)
